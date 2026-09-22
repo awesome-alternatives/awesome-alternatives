c26a2d62626edd3f3597e1fe31dc7511ebee9434
BryanFRD <bryanferrando59@gmail.com>
2026-09-22 19:10:30 +0200

    feat(site): allow https images for the README tab

--- site/src/lib/csp.ts
+++ site/src/lib/csp.ts
@@ -1,9 +1,9 @@
 export function cspDirectives(apiUrl: string | undefined): string[] {
   const connect = ["'self'"];
   if (apiUrl && URL.canParse(apiUrl)) connect.push(new URL(apiUrl).origin);
-  // TODO: add "img-src 'self' https:" when tool pages render the README and security HTML from the API.
   return [
     "default-src 'self'",
+    "img-src 'self' https: data:",
     "font-src 'self' data:",
     `connect-src ${connect.join(" ")}`,
     "object-src 'none'",
