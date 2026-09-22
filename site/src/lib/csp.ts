0640878c917f44a0cf93dfd2a5046a762032ec51
BryanFRD <bryanferrando59@gmail.com>
2026-09-22 19:06:11 +0200

    chore(site): flag the img-src change the README rendering will need

--- site/src/lib/csp.ts
+++ site/src/lib/csp.ts
@@ -1,6 +1,7 @@
 export function cspDirectives(apiUrl: string | undefined): string[] {
   const connect = ["'self'"];
   if (apiUrl && URL.canParse(apiUrl)) connect.push(new URL(apiUrl).origin);
+  // TODO: add "img-src 'self' https:" when tool pages render the README and security HTML from the API.
   return [
     "default-src 'self'",
     "font-src 'self' data:",
